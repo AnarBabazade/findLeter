a0b2f6de2a2584331a1722d68b9efada0c41bca0
AnarBabazade <babazadeanar94@gmail.com>
2024-01-18 00:09:30 +0400

    first commit

---
+++ main.js
@@ -0,0 +1,31 @@
+let soz=prompt("axtaris edeceyiniz sozu daxil edin")
+let herf=prompt("axtaris edeceyiniz herfi daxil edin")
+ result=soz.includes(herf);
+ if(result==true){
+    console.log("vardir");
+ }else{
+    console.log("yoxdur");
+ }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
